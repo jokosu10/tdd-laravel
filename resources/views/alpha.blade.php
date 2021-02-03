34592bdea7d7ea575dcbbafe0559370538a276e5
Joko Susilo <susilo.j8@gmail.com>
2021-02-03 12:24:45 +0700

    add unit testing using routing in web

---
+++ resources/views/alpha.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Alpha</title>
+</head>
+
+<body>
+	<h1>This is the Alpha page</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/alpha', function () {
+    return view('alpha');
+});
+
+Route::get('/beta', function () {
+    return view('beta');
+});
